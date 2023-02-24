1ab62ff4d0b8e14285427fcdacf14e00c8d1da01
MSkrzypietz <michael.skr97@gmail.com>
2023-02-24 13:25:02 +0100

    Fix email env variable

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -1,15 +1,21 @@
 import mailchannelsPlugin from "@cloudflare/pages-plugin-mailchannels";
 
-export const onRequest = mailchannelsPlugin({
-    personalizations: [
-        {
-            to: [{name: "ACME Support", email: process.env.EMAIL!}],
-        },
-    ],
-    from: {name: "Enquiry", email: process.env.EMAIL!},
-    respondWith: () =>
-        new Response(null, {
-            status: 302,
-            headers: {Location: "/thank-you"},
-        }),
-});
+interface Env {
+    EMAIL: string;
+}
+
+export const onRequest: PagesFunction<Env> = (context) =>
+    mailchannelsPlugin({
+        personalizations: [
+            {
+                to: [{name: "ACME Support", email: context.env.EMAIL}],
+            },
+        ],
+        from: {name: "Enquiry", email: context.env.EMAIL},
+        respondWith: () =>
+            new Response(null, {
+                status: 302,
+                headers: {Location: "/thank-you"},
+            }),
+    })(context);
+
